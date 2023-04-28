76f125b66d070eee67dbeec1b0564c1be461194d
Gorodeckii912 <d.gavrilenko912@gmail.com>
2023-04-28 21:34:33 +0100

    fin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { RequestServer } from './requestServer';
-import { markup } from './markup';
-import { LoadMoreButton } from './loadMoreButton';
+import axios from 'axios';
 
 const refs = {
     form: document.querySelector('#search-form'),
@@ -11,6 +9,40 @@ const refs = {
     loadMoreButton: document.querySelector('.load-more'),
     searchButton: document.querySelector('#search-form button'),
 };
+
+class LoadMoreButton {
+    constructor({ isHiden, disabled, loading, buttonAdress }) {
+        this.isHiden = isHiden;
+        this.disabled = disabled;
+        this.loading = loading;
+        this.buttonAdress = buttonAdress;
+    }
+    buttonState({ isHiden = this.isHiden, disabled = this.disabled, loading = this.loading }) {
+        isHiden ? this.buttonAdress.classList.add('visually-hidden') : this.buttonAdress.classList.remove('visually-hidden');
+        disabled ? this.buttonAdress.disabled = true : this.buttonAdress.disabled = false;
+        loading ? this.buttonAdress.textContent = 'Loading...' : this.buttonAdress.textContent = 'Load-more';
+    }
+}
+
+class RequestServer {
+    static url = 'https://pixabay.com/api/';
+    params = {
+        key: '35337679-b7947e609f482c58d47f4cd5a',
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: 0,
+        per_page: 40
+    };
+    onRequestServer(query = this.params.q) {
+        this.params.q = query;
+        this.params.page += 1;
+        return axios.get(RequestServer.url, { params: this.params });
+}  
+}
+
+
 const requestServer = new RequestServer();
 const onloadMoreButton = new LoadMoreButton({
     isHiden: true,
@@ -18,7 +50,30 @@ const onloadMoreButton = new LoadMoreButton({
     loading: false,
     buttonAdress: refs.loadMoreButton
 });
-let totalImagesUploaded = 40;    
+let totalImagesUploaded = 50;    
+
+function markup(array) {
+    return array.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
+        `<div class="gallery__item">
+     <a class="gallery__link" href="${largeImageURL}">
+  <img class="gallery__image" src="${webformatURL}" alt="${tags}"/>
+</a>
+<div class="gallery__info">
+    <p class="gallery__info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="gallery__info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="gallery__info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="gallery__info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+    </div>
+  </div>`).join(""); 
+}        
 
 onloadMoreButton.buttonState({});
 
@@ -110,15 +165,10 @@ async function onSumbitLoadMore(event) {
             
     } catch (error) {
         console.log(error);
-        // if (error.code === 'ERR_BAD_REQUEST') {
-        // Notify.warning("We're sorry, but you've reached the end of search results.");
-        // onloadMoreButton.buttonState({
-        // isHiden: true,
-        // disabled: true,
-        // });
-        // };      
+            
   };
 }
+
 
 
 
